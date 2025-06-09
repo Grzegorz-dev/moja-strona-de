0c170d06ee5001eed0c7e74d85914e62e6885558
Grzegorz <slowiaczek.grzegorz@gmail.com>
2025-06-09 16:03:56 +0200

    Poprawki tagi

--- src/app/api/wycena/route.ts
+++ src/app/api/wycena/route.ts
@@ -2,35 +2,29 @@ import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 export async function POST(req: Request) {
-  try {
-    const body = await req.json();
-    const { phone } = body;
+  const body = await req.json();
+  const { phone } = body;
 
-    if (!phone) {
-      return NextResponse.json({ ok: false, error: "Brak numeru telefonu" }, { status: 400 });
-    }
-
-    const transporter = nodemailer.createTransport({
-      host: "smtp.zoho.eu",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"Wycena" <${process.env.MAIL_USER}>`,
-      to: process.env.MAIL_RECEIVER,
-      subject: "Prośba o darmową wycenę",
-      html: `<p><b>Numer telefonu:</b> ${phone}</p>`,
-    });
-
-    return NextResponse.json({ ok: true });
-  } catch (error) {
-    console.error("❌ Błąd serwera:", error);
-    return NextResponse.json({ ok: false, error: "Błąd serwera" }, { status: 500 });
+  if (!phone) {
+    return NextResponse.json({ ok: false, error: "Brak numeru telefonu" }, { status: 400 });
   }
-}
 
+  const transporter = nodemailer.createTransport({
+    host: "mail.slowiaczek.pl",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USER, // kontakt@slowiaczek.pl
+      pass: process.env.MAIL_PASS  // wpisz to samo hasło, co do logowania do roundcube
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Wycena" <${process.env.MAIL_USER}>`,
+    to: process.env.MAIL_RECEIVER,
+    subject: "Prośba o darmową wycenę",
+    html: `<p><b>Numer telefonu:</b> ${phone}</p>`,
+  });
+
+  return NextResponse.json({ ok: true });
+}
