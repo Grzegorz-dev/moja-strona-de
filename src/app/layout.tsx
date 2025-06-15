a7b7deae243e9985d84c63ba32177a8d78b2a9c1
Grzegorz <slowiaczek.grzegorz@gmail.com>
2025-06-15 11:56:31 +0200

    Poprawki robots,sitemap,ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Menu from "./Menu";
 import AnimationWrapper from "./AnimationWrapper";
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head'
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -17,7 +18,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl">
+        <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=GTM-P8RFGTW9'+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P8RFGTW9');`,
+          }}
+        />
+      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P8RFGTW9"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <AnimationWrapper>
           <Menu />
           {children}
